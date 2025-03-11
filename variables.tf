a8ce84eaae350e16b53110200ab448ef5a7743ab
FlaviaBomfim <flavia.bomfim@icloud.com>
2025-03-10 21:48:58 -0300

    modified variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "atlas_public_key" {
   description = "Sua chave pública do MongoDB Atlas"
   type        = string
-  default       = "xxxx"
+
 }
 
 variable "atlas_private_key" {
   description = "Sua chave privada do MongoDB Atlas"
   type        = string
   sensitive   = true
-  default       = "xxxxx"
+
 }
 
 variable "project_id" {
   description = "ID do seu projeto no MongoDB Atlas"
   type        = string
-  default       =  "xxxxxx"
+
 }
